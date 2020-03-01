634ccd6ae56fbd37c44c484ccfae76d582d2a85b
TheHawksur20 <n.peiffer@ludus-academie.com>
2020-03-01 11:50:56 +0100

    no message

---
+++ main.c
@@ -0,0 +1,41 @@
+//Réponse aux 5 premières questions.
+
+//1) Qu’est-ce que la SDL 2 ?
+//-La SDL2 est une bibliothèque de développement
+
+//2) Que peut faire la SDL 2 ?
+//-Elle permet de gérer des interfaces graphiques, audios et les entrées utilisateurs et donc, entres autres, de faire des jeux
+
+//3) Sur quelles plateformes fonctionne la SDL 2 ?
+//-SDL2 fonctionne sur Windows/Mac/Android/iOS
+
+//4) Comment obtenir et installer la SDL 2 sur un EDI ? Donnez les étapes de la configuration d’un projet SDL2 avec CodeBlocks (gcc).
+//-Pour installer SDL2 sur Codeblocks, il faut télécharger SDL2 depuis le site officiel, la placer dans son dossier CodeBlocks, créer un nouveau projet SDL2 et ne pas oublier de linker les deux
+
+//5) Quelles sont les différences entre SDL 1 et SDL 2 ?
+//Voici la liste des principales nouvelles fonctionnaités:
+
+//- accélération matérielle 3D complète ;
+//- support d'OpenGL 3.0 et supérieur avec de nombreux profils (core, compatibility, debug, robust, etc.) ;
+//- support d'OpenGL ES ;
+//- support de multiples fenêtres ;
+//- support de multiples moniteurs ;
+//- support de multiples périphériques audio ;
+//- support d'Android et iOS ;
+//- bibliothèque de rendus simple 2D pouvant utiliser Direct3D, OpenGL, OpenGL ES ou un rendu logiciel ;
+//- retour de force disponible sur Windows, Mac OS X et Linux ;
+//- support de XInput et XAudio 2 pour Windows ;
+//- opérations atomiques ;
+//- gestion de l'énergie (indique l'autonomie de la batterie, etc.) ;
+//- fenêtre aux formes personnalisées ;
+//- audio 32 bits (entier et virgule flottante) ;
+//- bibliothèque simplifiée pour les manettes de jeu (la bibliothèque Joystick est toujours là !) ;
+//- support du toucher (multi-touch, mouvements, etc.) ;
+//- meilleur support du mode plein écran ;
+//- meilleur support du clavier (scancodes VS keycodes, etc.) ;
+//- boîtes de messages ;
+//- support du presse-papier ;
+//- support basique du glisser/déposer ;
+//- support correct des entrées Unicode et IME ;
+//- une macro puissante d'assertion ;
+//- licence zlib à la place de la LGPL ;
