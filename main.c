67eeb2ac7a83b7f71a88fd7d7999df4c08f14d1d
TheHawksur20 <n.peiffer@ludus-academie.com>
2020-03-01 12:46:31 +0100

    Réponse à la Question 8

--- main.c
+++ main.c
@@ -1,15 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <SDL2\SDL.h>
+//Voici les différentes primitives associees a la fenetre SDL2
 
-int main(int arc, char *argv[])
-{
-    SDL_Window *pWindow=NULL; //D'abord, on crée un pointeur fenetre
-
-    if (SDL_Init(SDL_INIT_EVERYTHING)>=0)
-    {
-        pWindow = SDL_CreateWindow("Fenetre de Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 780, SDL_WINDOW_SHOWN);
-        //Ensuite, on crée la fenetre grâce a SDL_CreateWindow qui prend en argument: Le titre, La position en x, La position en y, la Largeur, la Hauteur et les différents flags
-    }
-    return 0;
-}
+//SDL_DestroyWindow       -> Permet de détruire la fenêtre et libérer l'espace mémoire alloué
+//SDL_Delay               -> Sert a mettre en pause le programme grace a un argument, le nombre de millisecondes pendant lequel le programme reste en pause
+//SDL_SetWindowTitle      -> Changer le titre de la fenetre
+//SDL_SetWindowIcon       -> Définir une icone de fenetre
+//SDL_GetWindowTitle      -> Récupérer le titre de la fenetre
+//SDL_SetWindowPosition   -> Definir la position de la fenetre
+//SDL_SetWindowSize       -> Definir la taille de la fenetre
+//SDL_GetWindowPosition   -> Récuperer la position de la fenetre
+//SDL_GetWindowSize       -> Recuperer la taille de la fenetre
+//SDL_MaximizeWindow      -> Agrandissement de la fenetre
+//SDL_MinimizeWindow      -> Reduction de la fenetre
+//SDL_RestoreWindow       -> Restaurer la fenetre
+//SDL_SetWindowFullscreen -> Mettre la fenetre en plein ecran
+//SDL_SetWindowBrightness -> Definir la luminosite de la fenetre
+//SDL_GetWindowBrightness -> Recuperer la luminosite de la fenetre
