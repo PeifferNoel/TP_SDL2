adc52fcb155d2e620ce6027d13311e7973892796
TheHawksur20 <n.peiffer@ludus-academie.com>
2020-03-01 13:11:50 +0100

    no message

--- main.c
+++ main.c
@@ -1,17 +1,33 @@
-//Voici les différentes primitives associees a la fenetre SDL2
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2\SDL.h>
 
-//SDL_DestroyWindow       -> Permet de détruire la fenêtre et libérer l'espace mémoire alloué
-//SDL_Delay               -> Sert a mettre en pause le programme grace a un argument, le nombre de millisecondes pendant lequel le programme reste en pause
-//SDL_SetWindowTitle      -> Changer le titre de la fenetre
-//SDL_SetWindowIcon       -> Définir une icone de fenetre
-//SDL_GetWindowTitle      -> Récupérer le titre de la fenetre
-//SDL_SetWindowPosition   -> Definir la position de la fenetre
-//SDL_SetWindowSize       -> Definir la taille de la fenetre
-//SDL_GetWindowPosition   -> Récuperer la position de la fenetre
-//SDL_GetWindowSize       -> Recuperer la taille de la fenetre
-//SDL_MaximizeWindow      -> Agrandissement de la fenetre
-//SDL_MinimizeWindow      -> Reduction de la fenetre
-//SDL_RestoreWindow       -> Restaurer la fenetre
-//SDL_SetWindowFullscreen -> Mettre la fenetre en plein ecran
-//SDL_SetWindowBrightness -> Definir la luminosite de la fenetre
-//SDL_GetWindowBrightness -> Recuperer la luminosite de la fenetre
+int main(int arc, char *argv[])
+{
+    SDL_Window *pWindow=NULL;
+    SDL_Renderer *pRenderer=NULL; //Cette fois-ci, on cree aussi un renderer
+
+    //Un renderer est un espace de rendu de la fenetre, c'est à dire l'endroit ou l'on va executer les instructions avant de les afficher dans la fenetre.
+
+    if (SDL_Init(SDL_INIT_EVERYTHING)>=0)
+    {
+        pWindow=SDL_CreateWindow("Fenetre de Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 780, SDL_WINDOW_SHOWN);
+
+        if (pWindow != NULL)
+        {
+            pRenderer=SDL_CreateRenderer(pWindow, -1, SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC);
+            //On cree un renderer grace a SDL_CreateRenderer qui a comme arguments la fenetre a laquelle le renderer est associe, son index et ses flags
+
+            SDL_RenderPresent(pRenderer); //On "envoie" les instructions du renderer a la fenetre
+            SDL_Delay(2000);
+
+            SDL_DestroyRenderer(pRenderer); //On detruit le renderer
+            SDL_DestroyWindow(pWindow);
+            SDL_Quit();
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
