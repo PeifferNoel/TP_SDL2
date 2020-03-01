f2cd4a4ec00730e5d5b094ae76c5ee8c0b645407
TheHawksur20 <n.peiffer@ludus-academie.com>
2020-03-01 15:38:59 +0100

    Réponse à la Question 15

--- main.c
+++ main.c
@@ -1,2 +1,2 @@
-//Pour dessiner un point, on utilise SDL_RenderDrawPoint, qui prend en paramètre le renderer, la position en x et la position en y
-//Pour dessiner une ligne, on utilise SDL_RenderDrawLine, qui prend en paramètre le renderer, la position en x du point de départ, sa position en y, la position en x du point d'arrivée et sa position en y
+//La fonction SDL_RenderClear sert à remplir le renderer avec la couleur qu'on sélectionne avec SDL_SetRenderDrawColor
+//La fonction SDL_RenderPresent sert à envoyer à la fenetre les instructions du renderer, on appelle ça "Faire le rendu"
