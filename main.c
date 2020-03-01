f8a5ebb394aea3d10e93e9466759e0137d3274e3
TheHawksur20 <n.peiffer@ludus-academie.com>
2020-03-01 14:35:04 +0100

    Réponse à la Question 10

--- main.c
+++ main.c
@@ -1,33 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <SDL2\SDL.h>
-
-int main(int arc, char *argv[])
-{
-    SDL_Window *pWindow=NULL;
-    SDL_Renderer *pRenderer=NULL; //Cette fois-ci, on cree aussi un renderer
-
-    //Un renderer est un espace de rendu de la fenetre, c'est à dire l'endroit ou l'on va executer les instructions avant de les afficher dans la fenetre.
-
-    if (SDL_Init(SDL_INIT_EVERYTHING)>=0)
-    {
-        pWindow=SDL_CreateWindow("Fenetre de Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 780, SDL_WINDOW_SHOWN);
-
-        if (pWindow != NULL)
-        {
-            pRenderer=SDL_CreateRenderer(pWindow, -1, SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC);
-            //On cree un renderer grace a SDL_CreateRenderer qui a comme arguments la fenetre a laquelle le renderer est associe, son index et ses flags
-
-            SDL_RenderPresent(pRenderer); //On envoie les instructions du renderer a la fenetre
-            SDL_Delay(2000);
-
-            SDL_DestroyRenderer(pRenderer); //On detruit le renderer
-            SDL_DestroyWindow(pWindow);
-            SDL_Quit();
-        }
-        else
-        {
-            return 0;
-        }
-    }
-}
+//SDL_RenderDrawPoint(pRenderer, 50, 50); -> Sert à dessiner un point (Renderer selectionné, position en x du point, position en y)
+//Pour les rectangles, on doit d'abord les déclarer avant de les utiliser:
+//SDL_Rect nomDuRectangle = {point de départ en x , point de départ en y, largeur, hauteur};
+//Ensuite, on utilise SDL_RenderDrawRect, qui attends 2 paramètres: le renderer et le rectangle que l'on souhaite afficher
