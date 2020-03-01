2def3fcf445367f232c4bebb9da6310879b42c72
TheHawksur20 <n.peiffer@ludus-academie.com>
2020-03-01 14:39:28 +0100

    Réponse à la Question 11

--- main.c
+++ main.c
@@ -1,4 +1,1 @@
-//SDL_RenderDrawPoint(pRenderer, 50, 50); -> Sert à dessiner un point (Renderer selectionné, position en x du point, position en y)
-//Pour les rectangles, on doit d'abord les déclarer avant de les utiliser:
-//SDL_Rect nomDuRectangle = {point de départ en x , point de départ en y, largeur, hauteur};
-//Ensuite, on utilise SDL_RenderDrawRect, qui attends 2 paramètres: le renderer et le rectangle que l'on souhaite afficher
+//Pour gérer la couleur, on utilise SDL_SetRenderDrawColor, qui attend les paramètres RGBA (A étant la transparence de la couleur)
